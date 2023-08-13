9402d4a999f928bdcd2c53b2d3b05ff6540c1a44
Somil <somil0014@gmail.com>
2023-08-13 12:12:26 +0530

    added rosserial

--- build/beginner_tutorials/catkin_generated/installspace/my_odom.py
+++ build/beginner_tutorials/catkin_generated/installspace/my_odom.py
@@ -12,20 +12,32 @@ class odom_task:
 
     #self
     def __init__(self):
+
+        rospy.init_node('my_odom') #node name
+
         self.x=0
         self.y=0
         self.th=0
-        self.sample_rate=100
-        self.dt = 0.01
+        self.sample_rate=10
+        self.dt = 0.1
 
         #subcriber
         rospy.Subscriber("/cmd_vel", Twist, self.odomCallback)
     
     def odomCallback(self, msg):
 
-        # phi = (math.pi - msg.angular.z * self.dt)/2
-        # delta_x = (msg.linear.x * self.dt) * math.sin(se)
-        rospy.loginfo("%f", msg.angular.z)
+        # phi = (math.pi + msg.angular.z * self.dt)/2
+        # delta_x = (msg.linear.x * self.dt) * math.sin(phi + self.th - (math.pi /2))
+        # delta_y = (msg.linear.x * self.dt) * math.cos(phi + self.th - (math.pi /2))
+        # delta_th = msg.angular.z * self.dt
+        delta_x = msg.linear.x * self.dt * math.cos(self.th) 
+        delta_y = msg.linear.x * self.dt * math.sin(self.th) 
+        delta_th = msg.angular.z * self.dt 
+
+        self.x += delta_x
+        self.y += delta_y
+        self.th += delta_th
+        rospy.loginfo("x=%f , y=%f, th=%f", self.x, self.y, self.th)
 
     
 
@@ -35,8 +47,8 @@ if __name__ == '__main__':
     r = rospy.Rate(odom.sample_rate)
     while not rospy.is_shutdown():
         try:
-            odom.broadcast()
-            odom.publish()
+            # odom.broadcast()
+            # odom.publish()
             r.sleep()
         except rospy.exceptions.ROSTimeMovedBackwardsException:
             pass
